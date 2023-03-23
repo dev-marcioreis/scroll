873f41699effa552ae7039fb2cf24c05f8885dd2
Marcio Reis <dev.marcioreis@gmail.com>
2023-03-23 14:15:27 -0300

    Portfolio section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,43 @@
 window.addEventListener('scroll', () => {
 	document.documentElement.style.setProperty('--scrollTop', `${this.scrollY}px`)
 })
-gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
-ScrollSmoother.create({
-	wrapper: '.wrapper',
-	content: '.container'
-})
+
+if (ScrollTrigger.isTouch !== 1) {
+
+	ScrollSmoother.create({
+		wrapper: '.wrapper',
+		content: '.content',
+		smooth: 1.5,
+		effects: true
+	})
+
+	let itemsL = gsap.utils.toArray('.gallery__left .gallery__item')
+
+	itemsL.forEach(item => {
+		gsap.fromTo(item, { opacity: 0, x: -50 }, {
+			opacity: 1, x: 0,
+			scrollTrigger: {
+				trigger: item,
+				start: '-850',
+				end: '-100',
+				scrub: true
+			}
+		})
+	})
+
+	let itemsR = gsap.utils.toArray('.gallery__right .gallery__item')
+
+	itemsR.forEach(item => {
+		gsap.fromTo(item, { opacity: 0, x: 50 }, {
+			opacity: 1, x: 0,
+			scrollTrigger: {
+				trigger: item,
+				start: '-750',
+				end: 'top',
+				scrub: true
+			}
+		})
+	})
+
+}
+
